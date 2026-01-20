f06900cec4c50739cef8eb46428ad26dd6bd2cc8
danielle <silvasoaresdanielle2@gmail.com>
2026-01-19 23:12:46 -0300

    fix (backend): corrigindo erro de sintaxe no task model

--- backend/app/models/task.rb
+++ backend/app/models/task.rb
@@ -3,7 +3,7 @@ class Task < ApplicationRecord
   belongs_to :category
 
   validates :title, presence: true, length: {minimum: 4, maximum: 100}
-  validates :description, presence: true, , length: {maximum: 300}
+  validates :description, presence: true, length: {maximum: 300}
   validates :status, inclusion: { in: [true, false] }
   validates :due_date, presence: true
   validates :priority, inclusion: { in: [0, 1, 2] } # 0: low, 1: medium, 2: high
